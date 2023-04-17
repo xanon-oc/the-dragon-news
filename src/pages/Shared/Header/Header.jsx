738224131ded1e26af37e3d35a7f07d601a2c623
NILOY ROY <121684663+xanon-oc@users.noreply.github.com>
2023-04-18 00:25:13 +0600

    express server added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,44 @@
 import React from "react";
-
+import logo from "../../../assets/logo.png";
+import moment from "moment";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 const Header = () => {
   return (
-    <div>
-      <h2>This is Header</h2>
-    </div>
+    <Container>
+      <div className="text-center">
+        <img src={logo} alt="" />
+        <p className="text-secondary">
+          <small>Journalism Without Fear or Favor</small>
+          <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+        </p>
+      </div>
+      <div className="d-flex">
+        <Button variant="danger">Latest</Button>
+        <Marquee className="fw-fw-semibold" speed={50} pauseOnHover>
+          I can be a React component, multiple React components, or just some
+          text.
+        </Marquee>
+      </div>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+              <Nav.Link href="#pricing">Career</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Profile</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                <Button variant="dark">Login</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
   );
 };
 
---
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+const LeftNav = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+  return (
+    <div>
+      <h2>Left Nav</h2>
+    </div>
+  );
+};
+
+export default LeftNav;
